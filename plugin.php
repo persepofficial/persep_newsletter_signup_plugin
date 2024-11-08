25caf78d2d515d8fabea68f9b5dfb5d2027c1395
brandonjonesmckpr2 <brandon@persep.co>
2024-11-08 08:02:10 -0800

    Updated plugin details again. Missing the word 'a'

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Newsletter Sign Up
 * Plugin URI: https://www.persep.co/
-* Description: A Plugin that allows to add a newletter signup to website
+* Description: A Plugin that allows to add a newletter signup to a website
 * Version: 0.1
 * Author: Brandon Jones
 * Author URI: https://www.persep.co/
