61046304bd4af2cebe59fce65db81acbc58f3e45
brandonjonesmckpr2 <brandon@persep.co>
2024-11-07 19:43:56 -0800

    first commit

---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Plugin Name: test-plugin
+* Plugin URI: https://www.persep.co/
+* Description: Test.
+* Version: 0.1
+* Author: Brandon Jones
+* Author URI: https://www.persep.co/
+**/
+
+
+
